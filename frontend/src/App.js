04546e633c543caeb32666d535da55f8adf69bfc
Pranjali <pranjaligavhale02@gmail.com>
2023-10-11 00:43:18 +0530

    Adding files

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
+import Homepage from './Pages/Homepage';
+import Chatpage from './Pages/Chatpage';
+
+function App() {
+  return (
+    <div className="App">
+      <Route path='/' component={Homepage} exact/>
+      <Route path='/chat' component={Chatpage} />
+    </div>
+  );
+}
+
+export default App;
