f41b863a6b927341ed1eb0a547c259b97442fc9d
Pranjali Gavhale <pranjaligavhale02@gmail.com>
2023-10-28 15:07:28 +0530

    APIs

--- backend/server.js
+++ backend/server.js
@@ -3,24 +3,20 @@ const dotenv = require("dotenv");
 const { chats } = require("./data/data");
 const connectDB = require("./config/db");
 const colors = require("colors");
+const userRoutes = require("./routes/usersRoutes");
 
-const app = express();
 dotenv.config();
 connectDB();
+
+const app = express();
+
+app.use(express.json()); //to accept JSON data
 
 app.get("/", (req, res) => {
 	res.send("API is running successfully");
 });
 
-app.get("/api/chat", (req, res) => {
-	res.send(chats);
-});
-
-app.get("/api/chat/:id", (req, res) => {
-	//console.log(req.params.id);
-	const singleChat = chats.find((c) => c._id === req.params.id);
-	res.send(singleChat);
-});
+app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
